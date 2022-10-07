d7b3b9a9b054ec4e360ad4aaf051de2a638658b9
Joshi <joshi@Nigrutos-Macbook.local>
2022-10-08 04:31:09 +1100

    Corrected grammar and spelling

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -12,26 +12,26 @@ function Aboutme() {
       About me,  <i className="fa-solid fa-robot fa-bounce"></i></h1>
       <span aria-hidden="true">About me, <br/>  <br/> </span>
       <p> I majored in Health Information Management and Health Sciences before I chose <br/> 
-      to pursue this path to become a software dev. Before this I always considered <br/> 
+      to pursue this path to become a software dev. Before this, I always considered <br/> 
       computer science as I loved using computers in my childhood. While working as <br/>
       a Health Information Manager after graduation, the work was not as stimulating <br/>
-      as I wanted although, I worked with data manipulation using python scripts which <br/>
+      as I wanted, although I worked with data manipulation using python scripts which <br/>
       I really enjoyed which then brought me to discover my passion for software. <br/>
       I was indulged in the idea of the use of software to aid daily life. For example, <br/>
       MySpace, the idea that people can now be instantly connected with friends and <br/>
       family from all over the world and communicate only requiring an internet connection <br/>
       as apposed to old traditions before social media. This benefited the world immensely. <br/>
-      From this I decided to learn to program.
+      From this, I decided to learn to program.
       <br/>
       <br/>
-      Starting off on code academy I found it very difficult at first but after much resilence <br/> 
+      Starting off at code academy, I found it very difficult at first, but after much resilience <br/> 
       I began to really enjoy coding as I like to take on challenges and solve problems. <br/>
-      From code academy I developed a basic understanding of computer science expanding <br/>
+      From code academy I developed a basic understanding of computer science, expanding <br/>
       my knowledge to be able to learn from more challenging sources. From this I decided <br/>
       to start reading “The self taught programmer” by Cory Althoff, I learnt how to <br/>
       use GitHub from YouTube to be able to start uploading my learning journey, and <br/>
       decided to take on a big project, this website. My understanding of react is <br/>
-      limited however, with my basic understanding of software I was able to follow and <br/> 
+      limited, however, with my basic understanding of software, I was able to follow and <br/> 
       learn from tutorials. This experience was a big step forward from my knowledge as <br/>
       I wasn't able to understand very complex concepts. However, this sparked extra <br/>
       motivation for me to keep building upon my knowledge. <br/>
