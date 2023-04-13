e4778f218d4f7d9cb5f6bbd49adb495b81050e92
Josh <jthalappillil7@gmail.com>
2023-04-13 15:19:49 +1000

    Update aboutme.js
    Updated my About Me section

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -11,33 +11,23 @@ function Aboutme() {
       <span aria-hidden="true">About me, <br/>  <br/>  <i className="fa-solid fa-robot fa-bounce"></i> </span>
       About me,  <i className="fa-solid fa-robot fa-bounce"></i></h1>
       <span aria-hidden="true">About me, <br/>  <br/> </span>
-      <p> I majored in Health Information Management and Health Sciences before I chose <br/> 
-      to pursue this path to become a software dev. Before this, I always considered <br/> 
-      computer science as I loved using computers in my childhood. While working as <br/>
-      a Health Information Manager after graduation, the work was not as stimulating <br/>
-      as I wanted, although I worked with data manipulation using python scripts which <br/>
-      I really enjoyed which then brought me to discover my passion for software. <br/>
-      I was indulged in the idea of the use of software to aid daily life. For example, <br/>
-      MySpace, the idea that people can now be instantly connected with friends and <br/>
-      family from all over the world and communicate only requiring an internet connection <br/>
-      as apposed to old traditions before social media. This benefited the world immensely. <br/>
-      From this, I decided to learn to program.
-      <br/>
-      <br/>
-      Starting off at code academy, I found it very difficult at first, but after much resilience <br/> 
-      I began to really enjoy coding as I like to take on challenges and solve problems. <br/>
-      From code academy I developed a basic understanding of computer science, expanding <br/>
-      my knowledge to be able to learn from more challenging sources. From this I decided <br/>
-      to start reading “The self taught programmer” by Cory Althoff, I learnt how to <br/>
-      use GitHub from YouTube to be able to start uploading my learning journey, and <br/>
-      decided to take on a big project, this website. My understanding of react is <br/>
-      limited, however, with my basic understanding of software, I was able to follow and <br/> 
-      learn from tutorials. This experience was a big step forward from my knowledge as <br/>
-      I wasn't able to understand very complex concepts. However, this sparked extra <br/>
-      motivation for me to keep building upon my knowledge. <br/>
-      <br/>
-      I am extremely passionate about software and I continuously strive to increase  <br/>
-      my knowledge and learn new technologies to further my skills as an aspiring software developer.</p>
+      <p>
+        My journey into software development started after completing a degree in Health<br/> 
+        Information Management and Health Sciences. Though I worked briefly in the hospital, the<br/> 
+        work did not challenge me enough, and I started looking for something more engaging.<br/> 
+        During my degree, I had an introduction to SQL, which piqued my interest in software<br/> 
+        development. After leaving my job, I began my journey into software development by gaining<br/>
+        an understanding of Python through the book, "The Self-Taught Programmer"<br/> 
+        by Corey Althoff and challenged myself daily by completing Codewars challenges. Eventually,<br/> 
+        I moved on to The Odin Project, where I learned HTML, CSS, and JavaScript. Now, as a<br/> 
+        Graduate Software Engineer at Datacom, I'm expanding my knowledge by working in the<br/> 
+        .NET environment, specifically with C#. I have also begun completing small projects in C# as<br/>
+        seen on my GitHub. I have completed these projects whilst practicing Object Oriented Programming<br/>
+        to further enhance my skills as a developer. My ultimate goal is to work in the product<br/> 
+        development life cycle, so I continue to seek new opportunities to further my skills.<br/> 
+        Software development is my passion, and I'm committed to continuously learning and<br/> 
+        increasing my knowledge to become a better developer.
+      </p>
     </div>
   );
 }
